f8f3f42f8f0ae9f712d6bc1c439973f9f9af2110
jianfeng <497609288@qq.com>
2019-03-14 16:37:53 +0800

    add

---
+++ WisdomRouterKitDemo/WisdomRouterKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WisdomRouterKitDemo
+//
+//  Created by jianfeng on 2019/3/14.
+//  Copyright © 2019年 All over the sky star. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
